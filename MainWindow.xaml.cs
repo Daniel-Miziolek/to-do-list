3b571038423f88f932705262e1d1e4753d3f2f66
Daniel Miziołek <143220728+Daniel-Miziolek@users.noreply.github.com>
2024-01-19 23:37:04 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using MahApps.Metro.Controls;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetTodayDate();
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window = new Window1();
+            window.Show();
+            this.Close();
+
+        }
+
+        private void SetTodayDate()
+        {
+            
+            DateTime today = DateTime.Now;
+
+           
+            string formattedDate = today.ToString("MM/dd/yyyy");
+
+           
+            label.Content = formattedDate;
+        }
+
+        private void btn_delete_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you want to DELETE this task?","Question",MessageBoxButton.YesNo,MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                int choose = listBox1.SelectedIndex;
+                listBox1.Items.RemoveAt(choose);
+            }
+            else
+            {
+
+            }
+        }
+
+        private void btn_clear_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you want to CLEAR the list?", "Question", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                listBox1.Items.Clear();
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ToDoList
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            List<string> list = new List<string>
+            {
+                "Important",
+                "Not Important"
+            };
+            comboBox.ItemsSource = list;
+            text_date.IsReadOnly = true;
+            
+        }
+
+        private void btn_back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+            
+        }
+
+        private void calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (calendar.SelectedDate.HasValue)
+            {
+                string data = calendar.SelectedDate.Value.ToString();
+                text_date.Text = data;
+            }
+        }
+
+        private void text_title_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (text_title.Text == "")
+            {
+                text_title.Text = "Title";
+            }
+        }
+
+        private void text_title_GotFocus(object sender, RoutedEventArgs e)
+        {
+            text_title.Text = "";
+        }
+
+        private void text_des_GotFocus(object sender, RoutedEventArgs e)
+        {
+            text_des.Text = "";
+        }
+
+        private void text_des_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (text_des.Text == "")
+            {
+                text_des.Text = "Description";
+            }
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.listBox1.Items.Add($"Title: {text_title.Text}\nDescription: {text_des.Text}, Date: {text_date.Text}, Status: {comboBox.Text}");
+            main.Show();
+            this.Close();
+            
+        }
+    }
+}
